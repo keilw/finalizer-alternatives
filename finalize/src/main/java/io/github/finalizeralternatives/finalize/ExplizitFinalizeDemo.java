f54ba17c9a09c9653bf6282e891eeb2ce0d3f3c2
Werner Keil <werner.keil@gmx.net>
2022-07-07 12:18:23 +0200

    Refactoring

---
+++ finalize/src/main/java/io/github/finalizeralternatives/finalize/ExplizitFinalizeDemo.java
@@ -0,0 +1,22 @@
+package io.github.finalizeralternatives.finalize;
+
+public class ExplizitFinalizeDemo
+{
+    public static void main(String[] args) 
+    {
+        ExplizitFinalizeDemo demo1 = new ExplizitFinalizeDemo();
+        ExplizitFinalizeDemo demo2 = new ExplizitFinalizeDemo();
+        demo1 = null;
+        demo2.finalize(); // Explicit call to finalize method
+        System.out.println("Garbage collector aufgerufen");
+        System.gc(); //Implicit call to finalize() method
+        
+        
+    }
+    
+    @Override
+    protected void finalize()
+    {
+        System.out.println("finalize() Methode wird aufgerufen");
+    }
+}
